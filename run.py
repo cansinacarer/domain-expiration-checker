fbe91346e29353b44b2bda0e97b02c432dae4b8d
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-09-10 01:32:09 -0400

    changed the tracking to use updated_date instead of expiration_date

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from decouple import config
-from app import check_domain_expiration_date
+from app import check_domain_updated_date
 import time
 
 if __name__ == "__main__":
     domain = config("DOMAIN")
     while True:
-        check_domain_expiration_date(domain)
+        check_domain_updated_date(domain)
         time.sleep(60)
